c82756e523841f9b623dc314094b201cff2ddd1c
Krym4s <egolgiye@gmail.com>
2021-06-24 13:33:09 +0300

    russian alphabet added

---
+++ CyrillicCode.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+const char A    = -32;
+const char B    = -31;
+const char V    = -30;
+const char G    = -29;
+const char D    = -28;
+const char YE   = -27;
+const char J    = -26;
+const char Z    = -25;
+const char YI0  = -24;
+const char IY0  = -23;
+const char K    = -22;
+const char L    = -21;
+const char M    = -20;
+const char N    = -19;
+const char O    = -18;
+const char P    = -17;
+const char R    = -16;
+const char S    = -15;
+const char T    = -14;
+const char U    = -13;
+const char F    = -12;
+const char H    = -11;
+const char C    = -10;
+const char CH0  = -9;
+const char SH0  = -8;
+const char SH1  = -7;
+const char ZN0  = -6;
+const char Y    = -5;
+const char ZN1  = -4;
+const char E    = -3;
+const char YU0  = -2;
+const char YA0  = -1;
